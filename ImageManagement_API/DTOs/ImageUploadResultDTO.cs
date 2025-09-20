fd6207413aa4c5c937d0d238d1be763e5e199803
unknown <vinhntca181278@fpt.edu.vn>
2025-09-20 20:59:21 +0700

    imageapi

---
+++ ImageManagement_API/DTOs/ImageUploadResultDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace ImageManagement_API.DTOs
+{
+    public class ImageUploadResultDTO
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public string? Url { get; set; }
+        public string? PublicId { get; set; }
+    }
+}
---
+++ ImageManagement_API/Services/Interfaces/IImageUploadService.cs
@@ -0,0 +1,9 @@
+﻿using CloudinaryDotNet.Actions;
+
+namespace ImageManagement_API.Services.Interfaces
+{
+    public interface IImageUploadService
+    {
+        Task<ImageUploadResult> UploadAsync(IFormFile file, string folder = "pet_app");
+        Task<DeletionResult> DeleteAsync(string publicId);
+    }
